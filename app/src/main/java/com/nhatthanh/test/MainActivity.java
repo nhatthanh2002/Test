1e09434a44345867c6ceeafbc8515f884bd0b100
nhatthanh2002 <thanhjayce9944@gmail.com>
2022-11-21 10:24:48 +0700

    create valible float b

--- app/src/main/java/com/nhatthanh/test/MainActivity.java
+++ app/src/main/java/com/nhatthanh/test/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+float b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
